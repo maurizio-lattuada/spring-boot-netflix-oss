94ff65b51db7f45e1550dee5580d66dbac68211f
Maurizio Lattuada <maurizio.lattuada@vivates.ch>
2017-09-12 15:42:05 +0200

    Bumped up to SpringBoot 1.5.7.RELEASE

--- 05-spring-cloud-config/config-server/src/main/java/com/fortytwotalents/spring/cloud/workshop/ConfigServerApplication.java
+++ 05-spring-cloud-config/config-server/src/main/java/com/fortytwotalents/spring/cloud/workshop/ConfigServerApplication.java
@@ -2,9 +2,13 @@ package com.fortytwotalents.spring.cloud.workshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 // TODO 2: Enable the configuration server by adding @EnableConfigServer
+@EnableConfigServer
 // TODO 3: We also make sure that the config server is registered at Eureka with @EnableDiscoveryClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigServerApplication {
 
